c975620a1133cc4a0c72ce378d1615ed0cbfaf3b
Michel Lutegar <mlutegar@gmail.com>
2023-08-11 07:54:43 -0300

    feat: primeira aula

---
+++ Olá Mundo/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+// public: classe visivel para outras classes
+// class: é uma clase
+// static: os métodos não altera os atributos da classe
+// void: não retorna nada
+// 
